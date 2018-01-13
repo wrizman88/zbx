ff1d5bb3eb432baa92a2f66ccdf6a8ed4a2c1d21
wrizman88 <wrizman88@gmail.com>
2018-01-13 16:13:09 +0800

    Telegram Setting

---
+++ zbxtg_settings.py
@@ -0,0 +1,51 @@
+# -*- coding: utf-8 -*-
+
+tg_key = "491966823:AAG_Z3HQk3MXIzev-vYNTDU2NUYeHy6SEWg"  # telegram bot api key
+
+zbx_tg_prefix = "zbxtg"  # variable for separating text from script info
+zbx_tg_tmp_dir = "/var/tmp/" + zbx_tg_prefix  # directory for saving caches, uids, cookies, etc.
+zbx_tg_signature = False
+
+zbx_tg_update_messages = True
+zbx_tg_matches = {
+    "problem": "PROBLEM: ",
+    "ok": "OK: "
+}
+
+zbx_server = "http://10.71.0.240/zabbix/"  # zabbix server full url
+zbx_api_user = "Admin"
+zbx_api_pass = "zabbix"
+zbx_api_verify = True  # True - do not ignore self signed certificates, False - ignore
+
+zbx_basic_auth = False
+zbx_basic_auth_user = "Admin"
+zbx_basic_auth_pass = "zabbix"
+
+proxy_to_zbx = None
+proxy_to_tg = None
+
+#proxy_to_zbx = "proxy.local:3128"
+#proxy_to_tg = "proxy.local:3128"
+
+google_maps_api_key = None  # get your key, see https://developers.google.com/maps/documentation/geocoding/intro
+
+zbx_tg_daemon_enabled = False
+zbx_tg_daemon_wl_ids = [6931850, ]
+zbx_tg_daemon_wl_u = ["ableev", ]
+
+zbx_db_host = "localhost"
+zbx_db_database = "zabbix"
+zbx_db_user = "zbxtg"
+zbx_db_password = "zbxtg"
+
+
+emoji_map = {
+    "OK": "✅",
+    "PROBLEM": "❗",
+    "info": "ℹ️",
+    "WARNING": "⚠️",
+    "DISASTER": "❌",
+    "bomb": "💣",
+    "fire": "🔥",
+    "hankey": "💩",
+}
